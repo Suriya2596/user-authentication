89411585f440fc062e6a5914523f540fb053334c
suriya2496 <suriyajagan25@gmail.com>
2023-08-29 18:25:50 +0530

    changes in app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import axios from "axios"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [file,setFile] = React.useState({})
+
+  const handleFormSubmit = (e)=>{
+    e.preventDefault()
+    if(file){
+      console.log(file)
+      const formData = new FormData()
+      formData.append("file",file)
+      formData.append("name","sur")
+      axios.post("http://localhost:3450/api/user",formData)
+        .then((response)=>{
+          console.log(response)
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={handleFormSubmit}>
+          <input type='file' onChange={(e)=>{
+            setFile(e.target.files[0])
+          }} />
+          <input type='submit'/>
+        </form>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
